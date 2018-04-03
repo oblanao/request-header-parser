b167a43035470c735d83fc94189de65fb68b7dd7
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2018-04-03 14:19:34 +0300

    Finished response

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const os = require('os');
+const { detect } = require('detect-browser');
+const browser = detect();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    
+    let lang = req.headers['accept-language'].substr(0,5);
+    let ipAddress = req.connection.remoteAddress;
+    let software = `${os.platform()} ${os.release()} ${browser.name} v${browser.version}`;
+    console.log(req.headers['user-agent']);
+    res.send({
+        ipaddress: ipAddress,
+        language: lang,
+        software
+    }).end();
 });
 
 app.listen(port, () => {
